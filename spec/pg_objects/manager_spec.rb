d014116345cdea74d8e25cc5b0e16c23093e579f
Denis Kiselyov <denis.kiselev@interfax.ru>
2018-10-18 12:18:01 +0300

    rewrite manager tests with new fixtures

--- spec/pg_objects/manager_spec.rb
+++ spec/pg_objects/manager_spec.rb
@@ -1,9 +1,16 @@
 RSpec.describe PgObjects::Manager do
+  include FixtureHelpers
+
   let(:connection) { instance_double('Connection') }
   let(:ar) { ActiveRecord::Base }
+  let(:fixtures_path) { File.expand_path 'spec/fixtures/objects' }
+  let(:extension) { 'sql' }
 
   before do
     allow(ar).to receive(:connection) { connection }
+    allow(ar.connection).to receive(:adapter_name).and_return('PostgreSQL')
+    PgObjects.config.directories = [fixtures_path]
+    PgObjects.config.extensions = [extension]
   end
 
   describe 'db connection' do
@@ -13,32 +20,14 @@ RSpec.describe PgObjects::Manager do
     end
 
     it 'expects adapter to be postgres' do
-      allow(ar.connection).to receive(:adapter_name).and_return('PostgreSQL')
       expect { PgObjects::Manager.new }.not_to raise_error
     end
   end
 
   describe 'load files' do
-    before do
-      allow(ar.connection).to receive(:adapter_name).and_return('PostgreSQL')
-    end
-    
-    before do
-      fixtures_root_path = File.expand_path 'spec/fixtures/objects'
-      PgObjects.config.directories = [fixtures_root_path]
-
-      ['functions/1', 'functions/2', 'triggers/1', 'triggers/2'].each do |sub_path|
-        File.open [fixtures_root_path, sub_path, 'test.sql'].join('/'), 'w' do |file|
-          file << <<~SQL
-            SELECT 1;
-          SQL
-        end
-      end
-    end
-
     it 'loads 4 sql files in directory tree' do
       subject.load_files
-      expect(subject.objects.size).to eq 4
+      expect(subject.objects.size).to eq(fixtures_list(extension).size)
     end
   end
 end
