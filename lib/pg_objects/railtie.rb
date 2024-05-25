8d2b004b508cb8b40299da7ba3252cfb4359cee0
Denis Kiselev <denis.kiselyov@gmail.com>
2024-05-25 17:15:55 +0700

    Remove useless code

--- lib/pg_objects/railtie.rb
+++ lib/pg_objects/railtie.rb
@@ -1,9 +1,6 @@
 ##
 # Brings rake tasks to rails app
 class PgObjects::Railtie < Rails::Railtie
-  # initializer 'pg_objects.initialization' do |app|
-  # end
-
   rake_tasks do
     load 'tasks/pg_objects_tasks.rake'
   end
