398edc8cfd20b656df21d1bad9d7777859a66e80
Denis Kiselev <denis.kiselyov@gmail.com>
2024-06-12 11:53:07 +0300

    Add description for the generator

--- lib/generators/pg_objects/install/install_generator.rb
+++ lib/generators/pg_objects/install/install_generator.rb
@@ -1,6 +1,7 @@
 ##
 # Creates default directories structure
 class PgObjects::InstallGenerator < Rails::Generators::Base
+  desc 'Creates directories structure in `db` directory of the Rails application and initializes configuration files'
   def create_directories
     empty_directory 'db/objects'
     empty_directory 'db/objects/before'
