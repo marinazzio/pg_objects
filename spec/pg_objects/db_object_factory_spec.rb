2106e649281a655539ab3df399d14db74768a1d9
Denis Kiselev <denis.kiselyov@gmail.com>
2024-07-14 17:30:40 +0700

    Fix rubocop offence

--- spec/pg_objects/db_object_factory_spec.rb
+++ spec/pg_objects/db_object_factory_spec.rb
@@ -7,14 +7,13 @@ RSpec.describe PgObjects::DbObjectFactory do
   describe '#create_instance' do
     before do
       allow(PgObjects::DbObject).to receive(:new).and_call_original
-      allow_any_instance_of(PgObjects::DbObject).to receive(:create)
+      allow_any_instance_of(PgObjects::DbObject).to receive(:create).and_return(nil) # rubocop:disable RSpec/AnyInstance
     end
 
     it 'creates a new DbObject instance with the given path and status' do
-      expect(PgObjects::DbObject).to receive(:new).with(path, :new, parser:).and_call_original
-      expect_any_instance_of(PgObjects::DbObject).to receive(:create)
-
       subject.create_instance(path, status: :new)
+
+      expect(PgObjects::DbObject).to have_received(:new).with(path, :new, parser:)
     end
   end
 end
