67ff8e6e4b11cf739b03dbf2ba4472b7cfd4a731
Denis Kiselev <denis.kiselyov@gmail.com>
2025-08-19 17:18:35 +0700

    Resolve rubocop offences

--- spec/benchmark_spec.rb
+++ spec/benchmark_spec.rb
@@ -1,9 +1,11 @@
-RSpec.describe 'Benchmark script' do
+require 'English'
+
+RSpec.describe 'Benchmark script' do # rubocop:disable RSpec/DescribeClass
   let(:benchmark_script) { File.join(__dir__, '..', 'bin', 'benchmark') }
 
-  it 'runs without errors' do
+  it 'runs without errors', :aggregate_failures do
     output = `cd #{File.dirname(__dir__)} && bundle exec #{benchmark_script} --files 5 --quiet 2>&1`
-    expect($?.exitstatus).to eq(0)
+    expect($CHILD_STATUS.exitstatus).to eq(0)
     expect(output).to include('Benchmarking File I/O Operations')
     expect(output).to include('Benchmarking SQL Parsing')
     expect(output).to include('Benchmarking Dependency Extraction')
@@ -11,9 +13,9 @@ RSpec.describe 'Benchmark script' do
     expect(output).to include('Memory Usage Analysis')
   end
 
-  it 'displays help when --help is passed' do
+  it 'displays help when --help is passed', :aggregate_failures do
     output = `cd #{File.dirname(__dir__)} && bundle exec #{benchmark_script} --help 2>&1`
-    expect($?.exitstatus).to eq(0)
+    expect($CHILD_STATUS.exitstatus).to eq(0)
     expect(output).to include('Usage:')
     expect(output).to include('--files')
     expect(output).to include('--large-files')
@@ -21,12 +23,11 @@ RSpec.describe 'Benchmark script' do
     expect(output).to include('--quiet')
   end
 
-  it 'respects the --files option' do
+  it 'respects the --files option', :aggregate_failures do
     output = `cd #{File.dirname(__dir__)} && bundle exec #{benchmark_script} --files 10 --quiet 2>&1`
-    expect($?.exitstatus).to eq(0)
+    expect($CHILD_STATUS.exitstatus).to eq(0)
     # Should have at least the base SAMPLE_SQLS files (8) plus the large file (1)
     expect(output).to match(/Read \d+ files/)
     expect(output).to match(/Parsed \d+ files/)
   end
 end
-
