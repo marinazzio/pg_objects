4da47bc147ef49ae706ff7f6dab6a3f826207002
Denis Kiselev <denis.kiselyov@gmail.com>
2026-06-19 01:25:12 +0700

    Refactor DbObjectFactory specs to ensure create_instance invokes create on DbObject and uses parser

--- spec/pg_objects/db_object_factory_spec.rb
+++ spec/pg_objects/db_object_factory_spec.rb
@@ -1,17 +1,34 @@
 RSpec.describe PgObjects::DbObjectFactory do
-  subject { described_class.new(parser:) }
+  subject(:factory) { described_class.new(parser:) }
 
   let(:path) { 'path/to/db_object.sql' }
   let(:parser) { instance_double(PgObjects::Parser) }
+  let(:db_object) { instance_double(PgObjects::DbObject) }
 
   describe '#create_instance' do
     before do
-      allow(PgObjects::DbObject).to receive(:new).and_call_original
-      allow_any_instance_of(PgObjects::DbObject).to receive(:create).and_return(nil) # rubocop:disable RSpec/AnyInstance
+      allow(PgObjects::DbObject).to receive(:new).with(path, :new, parser:).and_return(db_object)
+      allow(db_object).to receive(:create).and_return(db_object)
     end
 
-    it 'creates a new DbObject instance with the given path and status' do
-      subject.create_instance(path, status: :new)
+    it 'creates a new DbObject instance with the given path, status and parser' do
+      factory.create_instance(path, status: :new)
+
+      expect(PgObjects::DbObject).to have_received(:new).with(path, :new, parser:)
+    end
+
+    it 'invokes #create on the DbObject' do
+      factory.create_instance(path, status: :new)
+
+      expect(db_object).to have_received(:create)
+    end
+
+    it 'returns the DbObject produced by #create' do
+      expect(factory.create_instance(path, status: :new)).to be(db_object)
+    end
+
+    it 'defaults the status to :new when none is given' do
+      factory.create_instance(path)
 
       expect(PgObjects::DbObject).to have_received(:new).with(path, :new, parser:)
     end
